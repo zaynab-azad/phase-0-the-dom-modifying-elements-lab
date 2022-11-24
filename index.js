ae4b8c58304880df3835643c9f8350cb7f593b09
Zaynab Azad Khdir <zaynabbbazad20@gmail.com>
2022-11-24 11:09:24 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector(`main`).remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "Abraham is the champion";
